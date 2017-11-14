a5600532b031305a6e5ff189399731d39a5aeba3
Noam Marko <noamma@nvgnet.co.il>
2017-11-14 16:40:15 +0200

    14.11.2017 update

--- class13112017/login.php
+++ class13112017/login.php
@@ -4,7 +4,7 @@
 <link rel="stylesheet" type="text/css" href="css/login.css">
 </head>
 <body>
-<form id="login" method="GET" action="server/authenticate.php">
+<form id="login" method="POST" action="server/authenticate.php">
 <fieldset>
 <input type="text" name="usr" placeholder="User Name" autofocus>
 <input type="password" name="psd" placeholder="Password">
--- class13112017/server/authenticate.php
+++ class13112017/server/authenticate.php
@@ -1,28 +1,39 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <link rel="stylesheet" type="text/css" href="css/authenticate.css">
+</head>
+<body>
 <?php
 $users = array("noam"=>"noam1","test"=>"test1");
-if(!empty($_GET['usr'])){
-  if(array_key_exists($_GET['usr'],$users)){
+$redstring = "<p class='redirect'> Redirecting back to login page in";
+$redstring .= "<span id='counter'> 3 </span>seconds<p>";
+
+if(!empty($_POST['usr'])){
+  if(array_key_exists($_POST['usr'],$users)){
     /*for(){
         
             }*/
-            if ($users[$_GET['usr']]=$_GET['psd']){
-                echo "Hello, ".$_GET['usr']."<br><br>You where succesfully authenticated!";
+            if ($users[$_POST['usr']]=$_POST['psd']){
+                echo "Hello, ".$_POST['usr']."<br><br>You where succesfully authenticated!";
             }else{
                 header('Refresh:3;url=../login.php');
-                echo "authentication Faield!<br>Redirecting back to login page in 3 seconds";
+                echo "authentication Faield!<br>".$redstring;
             }
             
             
   }  else{
       header('Refresh:3;url=../login.php');
-      echo "user ".$_GET['usr'].",  is not Authorized for login!<br>Redirecting back to login page in 3 seconds";
+      echo "user ".$_POST['usr'].",  is not Authorized for login!<br>Redirecting back to login page in 3 seconds";
       exit();  
   }
     
-   // print "Hello, ".$_GET['usr'];
+   // print "Hello, ".$_POST['usr'];
 }else{
     header('Refresh:3;url=../login.php');
     print "no user was submmited<br>Redirecting back to login page in 3 seconds";
     exit();
 }
 ?>
+</body>
+</html>
