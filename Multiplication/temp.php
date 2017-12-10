3cb81c55455beac67e5dcf01d133df52fc0de5d6
Noam Marko <noamma@nvgnet.co.il>
2017-12-11 00:18:16 +0200

    finished multiplication

---
+++ Multiplication/temp.php
@@ -0,0 +1,10 @@
+<h2><?php echo $msg ?><h2>
+<?php echo $question ?>
+
+<input type="hidden" name="num1" value=<?php echo $num1?>>
+<input type="hidden" name= "num2" value=<?php echo $num2?>>
+<input type="hidden" name= "result" value=<?php echo $result?>>
+<input type="hidden" name= "points"  value=<?php echo $points?>>
+<input type="hidden" name= "turn"  value=<?php echo $turn?>>
+<input type="text" placeholder="answer" name="myanswer">
+<?php echo $btn?>
